a2bddd31386bfbd4313da5ae51c075e92bb02101
Ian Hoffman <ianrhoffman1@gmail.com>
2022-03-16 16:47:31 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,12 +2,42 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
-
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE user (
+  id INT,
+  name VARCHAR(24),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE friends (
+  id INT,
+  name VARCHAR(24),
+  isFriend BOOLEAN NOT NULL DEFAULT 0,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rooms (
+  id INT,
+  roomname VARCHAR(16),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id INT,
+  text VARCHAR(60),
+  room_id INT,
+  userid INT,
+  friend_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (room_id)
+    REFERENCES rooms(id),
+  FOREIGN KEY (userid)
+    REFERENCES user(id),
+  FOREIGN KEY (friend_id)
+    REFERENCES friends(id)
+);
 
 
 
